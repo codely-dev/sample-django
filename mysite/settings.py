66a9d8f21fd3a746d79564d66c400cebb9afafed
jeremyknoepfel <jeremyknoepfel@hotmail.com>
2021-04-22 18:54:15 +0200

    x

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "events",
     "profiles",
     "pwa",
-    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
